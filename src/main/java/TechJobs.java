eca7934dd0b2ad5ea9c862c225cde6c42b2d66af
Nathan Murray <namurray1215@gmail.com>
2022-09-08 19:26:36 -0500

    working no tests passed 1.1

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -121,7 +121,7 @@ public class TechJobs {
             System.out.print("No Results");
         } else {
             for (int x = 0; x < someJobs.size(); x++) {
-                System.out.println();
+                System.out.println();//sae this could solve issue with\n?
                 System.out.println("*****");
                 for (Map.Entry<String, String> listing : someJobs.get(x).entrySet()) {
                     System.out.println(listing.getKey()
